6cc5967ad0d556eb84c480cc30eb211664dc6857
Dusan <masinacdt@gmail.com>
2019-02-25 16:24:07 +0100

    added whitespace

--- files.php
+++ files.php
@@ -4,11 +4,11 @@
 echo readfile("D:/xampp/htdocs/symphart/public/Files/info.txt");
 
 //Metoda za informaciju o velicini fajla (u bajtovima)
-$file_size=filesize("D:/xampp/htdocs/symphart/public/Files/info.txt");
+$file_size = filesize("D:/xampp/htdocs/symphart/public/Files/info.txt");
 
 //Metoda za informaciju o ekstenziji fajla
-$target_file="D:/xampp/htdocs/symphart/public/Files/info.txt";
-$file_extension=pathinfo($target_file, PATHINFO_EXTENSION);
+$target_file = "D:/xampp/htdocs/symphart/public/Files/info.txt";
+$file_extension = pathinfo($target_file, PATHINFO_EXTENSION);
 
 //Metoda za proveru postojanja fajla (non void metoda, vraca true ako postoji fajl)
 file_exists("D:/xampp/htdocs/symphart/public/Files/info.txt");
@@ -16,12 +16,12 @@ file_exists("D:/xampp/htdocs/symphart/public/Files/info.txt");
 //Metode kojae se cesce koriste za manipulaciju fajlova - fopen + fread/fwrite
 
 //READ
-$file=fopen("D:/xampp/htdocs/symphart/public/Files/info.txt","r") or die();
+$file = fopen("D:/xampp/htdocs/symphart/public/Files/info.txt","r") or die();
 echo fread($file,$file_size);
 fclose($file);
 
 //WRITE
-$file=fopen("D:/xampp/htdocs/symphart/public/Files/dt1.txt","a") or die();
+$file = fopen("D:/xampp/htdocs/symphart/public/Files/dt1.txt","a") or die();
 $text_to_write="To je tacna informacija";
 fwrite($file,$text_to_write);
 fclose($file);
